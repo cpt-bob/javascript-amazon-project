d18a9cd407daf0fc78c93182078876a39c66c8e7
ltbob <cpt.bob@gmail.com>
2024-05-30 14:08:15 -0600

    New quantites are being updated in the DOM

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,9 @@
-import { cart, removeFromCart, calculateCartQuantity } from "../data/cart.js";
+import {
+  cart,
+  removeFromCart,
+  calculateCartQuantity,
+  updateQuantity,
+} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -148,6 +153,6 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
       `.js-quantity-input-${productId}`
     );
     const newQuantity = Number(quantityInput.value);
-    console.log("🚀 ~ link.addEventListener ~ newQuantity:", newQuantity);
+    updateQuantity(productId, newQuantity);
   });
 });
